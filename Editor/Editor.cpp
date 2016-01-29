6a8c16654570a43cf1a9e1d7bb22e70393b67232
a15johah <a15johah@student.his.se>
2016-01-29 11:09:03 +0100

    Updating Editor as well
    Just make all the things the same

--- Editor/Editor.cpp
+++ Editor/Editor.cpp
@@ -32,32 +32,13 @@ Editor::~Editor(){
 }
 
 void Editor::run(){
-	/*
-		File API test
-	*/
-	File f;
-	f = f.parent();
-	cout << f.path() << ' ' << round(f.size() / 1024.0f) << "kB" << endl;
-	for (File c : f.listFiles()){
-		cout << "  " << c.name() << ' ' << round(c.size() / 1024.0f) << "kB" << endl;
-	}
-
-	/*
-		Configuration API test
-	*/
 	c::initialize();
-	printf("\n");
-	printf(c::textureDir.path().data());
-	printf("\n");
-
-	/*
-		SFML & Manager API test
-	*/
-	window = new RenderWindow(VideoMode(500, 500), "SFML works!");
+	gi::initalize(window);
 	manager = new Manager();
 	manager->initialize(window);
+	world = new World();
+
 	CircleShape shape(100.f);
-	shape.setFillColor(Color::Green);
 	shape.setOrigin(100, 100);
 
 	float dx = 0.0f;
@@ -68,26 +49,31 @@ void Editor::run(){
 	mouseMoveListenerId = manager->inputManager->registerListener(mouseMoveListenerW);
 	mouseWheelListenerId = manager->inputManager->registerListener(mouseWheelListenerW);
 
-	while (window->isOpen()){
-		manager->tick(window, 0, 0);
+	while (gi::startOfFrame()){
+		manager->tick(window, world->time(), world->dt());
 
 		window->clear();
 
 		dx += 0.0001f;
 		dy += 0.0001f;
-		shape.setPosition(250, 250);
+
+		shape.setPosition(640, 360);
 		shape.rotate(0.01f);
 		shape.setFillColor(Color(int(255 * abs(sin(dx))), int(255 * abs(cos(dy / 2))), int(255 * abs(cos(dy))), int(55 + 200.0f * abs(cos(dy)))));
 		shape.setScale(abs(sin(dx)), abs(cos(dy)));
 		window->draw(shape);
-		window->display();
+
+		gi::endOfFrame();
 	}
 	manager->finalize(window);
 	delete manager;
-	delete window;
+	gi::finalize();
+	delete world;
 }
 
 const void Editor::keyboardListener(KeyboardEvent event){
+	if (event.key() == Keyboard::Escape)
+		window->close();
 	cout << (event.pressed() ? "press " : "release ") << event.key() << " " << event.first() << endl;
 }
 
